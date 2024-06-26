66802f1e426354c170ecfe0477282c49c04cc809
elisheva <e0504116785@gmail.com>
2024-06-26 10:16:52 +0300

    first commit

---
+++ ‏‏WebApi/BLL/Interfaces/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using DTO.Classes;
+
+namespace BLL.Interfaces;
+
+public interface IUserBll
+{
+    User Register(User user);
+    User Login(string email, string password);
+    List<User> GetUsers();
+
+}
